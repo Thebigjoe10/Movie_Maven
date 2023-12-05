b4728c0ae3e49e0e15f3e989983baac90d544de5
thebigjoe10 <jojosr001@gmail.com>
2023-12-05 07:14:30 -0800

    updated home component by add tvshowlist component inside home component

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 import GenreMovieList from "../Constant/GenreMovieList";
 import MovieList from "../Components/MovieList";
 import Slider from "../Components/Slider";
+import TvshowList from "../Components/TvshowList";
+import MovieTvshowList from "../Constant/MovieTvshowList";
 export default function Home() {
 return(
   <div>
@@ -14,6 +16,19 @@ return(
           <h1 className="text-[20px] font-bold text-white">{item.name}
             </h1>
             <MovieList genreId={item.id} />
+            
+      </div>
+      )
+    }
+    <h1 className="text-[20px] font-bold text-white p-8 px-8 md:px-16">Tv Series</h1>
+    {
+      MovieTvshowList.genres.map((item, index)=> index<=5&&
+      <div
+      key={index}
+      className="p-8 px-8 md:px-16"
+      >
+        <h1 className="text-[20px] font-bold text-white">{item.name}</h1>
+        <TvshowList genresId={item.id}/>
       </div>
       )
     }
