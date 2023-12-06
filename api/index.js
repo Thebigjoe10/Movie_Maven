faeb084caf010e8dd70afd53ef6952d28d58268a
thebigjoe10 <jojosr001@gmail.com>
2023-12-06 05:27:12 -0800

    move folders

--- api/index.js
+++ api/index.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+ 
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
+
+
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
-});
+})
 
 app.listen(5000, () => console.log('Server running on port 5000'));
