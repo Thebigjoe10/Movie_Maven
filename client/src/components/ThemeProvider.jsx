ab485ae19697c2b4b3dffbff4964db5774c2674c
thebigjoe10 <jojosr001@gmail.com>
2024-01-07 14:44:44 -0800

    add redux, component and pages folders

---
+++ client/src/components/ThemeProvider.jsx
@@ -0,0 +1,12 @@
+import { useSelector } from 'react-redux';
+
+export default function ThemeProvider({ children }) {
+  const { theme } = useSelector((state) => state.theme);
+  return (
+    <div className={theme}>
+      <div className='dark-mode'>
+        {children}
+      </div>
+    </div>
+  );
+}
