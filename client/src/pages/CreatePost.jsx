a8799952b868d59ee6970becc86a49d813fd6b32
Josy <110489359+Thebigjoe10@users.noreply.github.com>
2024-01-21 19:55:12 +0100

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -150,8 +150,8 @@ export default function CreatePost() {
             <option value='movies'>Movies</option>
             <option value='series'>Series</option>
             <option value='Kdrama'>Kdrama</option>
-            <option value='news'>Anime</option>
-            <option value='anime'>News</option>
+            <option value='anime'>Anime</option>
+            <option value='news'>News</option>
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
