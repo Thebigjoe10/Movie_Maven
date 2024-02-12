665f5f5a80534574bb51c7efeffc61d4d07872d2
thebigjoe10 <jojosr001@gmail.com>
2024-02-12 10:26:20 -0800

    update PostCard.jsx

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,14 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function PostCard({ post }) {
   return (
-    <div className='group relative w-full md:w-1/2 lg:w-1/4 border border-teal-500 hover:border-2 h-[460px] md:h-auto overflow-hidden rounded-lg sm:w-[430px] transition-all'>
+    <div className='group relative w-1/3 md:w-1/3 lg:w-1/4 border border-teal-500 hover:border-2 overflow-hidden rounded-lg transition-all'>
       <Link to={`/post/${post.slug}`}>
-        <img
-          src={post.image}
-          alt='post cover'
-          className='h-full w-full object-cover transition-all duration-300 z-20'
-        />
+        <div className="aspect-w-4 aspect-h-5">
+          <img
+            src={post.image}
+            alt='post cover'
+            className='h-full w-full object-cover transition-all duration-300 z-20'
+          />
+        </div>
         <div className='p-3 flex flex-col gap-2 absolute bottom-0 left-0 right-0 bg-black bg-opacity-0 group-hover:bg-opacity-50 transition-all duration-300 opacity-0 group-hover:opacity-100'>
           <p className='text-lg font-semibold line-clamp-2 text-white'>{post.title}</p>
           <span className='italic text-sm text-white'>{post.category}</span>
