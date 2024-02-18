ddfc88fb2888bca0bcdc9ed7e69ec467da01968e
thebigjoe10 <jojosr001@gmail.com>
2024-02-18 08:00:03 -0800

    Update PostPage.jsx

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -11,18 +11,18 @@ export default function PostCard({ post }) {
             className='h-full w-full object-cover transition-all duration-300 z-20'
           />
         </div>
-        <div className='p-3 flex flex-col gap-2 absolute bottom-0 left-0 right-0 bg-black bg-opacity-0 group-hover:bg-opacity-50 transition-all duration-300 opacity-0 group-hover:opacity-100'>
-          <p className='text-lg font-semibold line-clamp-2 text-white'>{post.title}</p>
-          <span className='italic text-sm text-white'>{post.category}</span>
+      </Link>
+      <div className='p-3 flex flex-col gap-2'>
+        <p className='text-lg font-semibold line-clamp-2'>{post.title}</p>
+        <span className='italic text-sm text-white'>{post.category}</span>
           <span className='italic text-sm text-white'>{post.genre}</span>
-          <Link
+        <Link
             to={`/post/${post.slug}`}
             className='z-10 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'
           >
             Show More
           </Link>
-        </div>
-      </Link>
+      </div>
     </div>
   );
 }
