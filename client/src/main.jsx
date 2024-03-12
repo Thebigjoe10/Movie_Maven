efc469abdefab976924dde6e70fb472e0d3a18ec
thebigjoe10 <jojosr001@gmail.com>
2024-03-12 12:52:36 +0100

    Update main.jsx and index.html

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -92,6 +92,12 @@ const Main = () => {
           id="json-ld-script"
           type="application/ld+json"
         >{`[]`}</script>
+        <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="moviemaven1_bot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+<script type="text/javascript">
+  function onTelegramAuth(user) {
+    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+  }
+</script>
       </Helmet>
       <PersistGate persistor={persistor}>
         <Provider store={store}>
