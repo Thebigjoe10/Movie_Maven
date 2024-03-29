0a1bdd502f8ed2ff37661f2e6e4f7ae69a9878fc
Josy <110489359+Thebigjoe10@users.noreply.github.com>
2024-03-29 13:21:45 +0100

    Update babel.config.js

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,3 +1,9 @@
-{
-  "plugins": ["@babel/plugin-proposal-optional-chaining"]
-}
+module.exports = {
+  presets: [
+    '@babel/preset-env', // Include this preset to transpile modern JavaScript syntax to a compatible version
+    '@babel/preset-react' // Include this preset to transpile JSX syntax
+  ],
+  plugins: [
+    '@babel/plugin-proposal-optional-chaining' // Include this plugin to transpile optional chaining syntax
+  ]
+};
