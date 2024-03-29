f997969c44050f382ec300f94b92e4c61d9777be
Josy <110489359+Thebigjoe10@users.noreply.github.com>
2024-03-29 14:09:48 +0100

    Delete client/babel.config.js

--- client/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: [
-    '@babel/preset-env', // Include this preset to transpile modern JavaScript syntax to a compatible version
-    '@babel/preset-react' // Include this preset to transpile JSX syntax
-  ],
-  plugins: [
-    '@babel/plugin-proposal-optional-chaining' // Include this plugin to transpile optional chaining syntax
-  ]
-};
