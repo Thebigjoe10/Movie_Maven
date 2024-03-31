9e2439177b0fec21217d9aa9214cc65bf08116d1
thebigjoe10 <jojosr001@gmail.com>
2024-03-31 01:00:46 +0100

    update index.js

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import prerender from 'prerender-node';
 
 dotenv.config();
 
@@ -18,22 +19,24 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-const __dirname = path.resolve();
-
-const app = express();
-
-app.use(express.json());
-app.use(cookieParser());
+  
+  const __dirname = path.resolve();
+  
+  const app = express();
+  
+  app.use(express.json());
+  app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
+app.use(prerender.set('prerenderToken', 'R3bn0UYo49fq41xCWvtj'));
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
