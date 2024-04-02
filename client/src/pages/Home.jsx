c96eb48abf77b0b9270334e66c22e38db221ca09
Josy <110489359+Thebigjoe10@users.noreply.github.com>
2024-04-02 22:44:37 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,7 +24,7 @@ export default function Home() {
     const fetchPostsByCategory = async (category, setPosts) => {
       try {
         const res = await fetch(
-          `/api/post/getposts?category=${category}&limit=3`
+          `/api/post/gethomepageposts?category=${category}&limit=3`
         );
         const data = await res.json();
         setPosts(data.posts);
