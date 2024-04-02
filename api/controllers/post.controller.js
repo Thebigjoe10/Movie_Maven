5c3170b82d9779c7aeb792d421835a37ec442846
Josy <110489359+Thebigjoe10@users.noreply.github.com>
2024-04-02 22:14:49 +0100

    Update post.controller.js

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -30,24 +30,24 @@ export const getposts = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 12;
-    const sortDirection = req.query.order === 'asc' ? 1 : -1;
-    const posts = await Post.find({
-      ...(req.query.userId && { userId: req.query.userId }),
-      ...(req.query.category && { category: req.query.category }),
-      ...(req.query.genre && { genre: req.query.genre }),
-      ...(req.query.slug && { slug: req.query.slug }),
-      ...(req.query.postId && { _id: req.query.postId }),
-      ...(req.query.searchTerm && {
-        $or: [
-          { title: { $regex: req.query.searchTerm, $options: 'i' } },
-          { content: { $regex: req.query.searchTerm, $options: 'i' } },
-          
-        ],
-      }),
-    })
-    .sort({ updatedAt: sortDirection })
-    .skip(startIndex)
-    .limit(limit);
+    const posts = await Post.aggregate([
+      {
+        $match: {
+          ...(req.query.userId && { userId: req.query.userId }),
+          ...(req.query.category && { category: req.query.category }),
+          ...(req.query.genre && { genre: req.query.genre }),
+          ...(req.query.slug && { slug: req.query.slug }),
+          ...(req.query.postId && { _id: req.query.postId }),
+          ...(req.query.searchTerm && {
+            $or: [
+              { title: { $regex: req.query.searchTerm, $options: 'i' } },
+              { content: { $regex: req.query.searchTerm, $options: 'i' } },
+            ],
+          }),
+        },
+      },
+      { $sample: { size: limit } } // Specify the number of documents you want to retrieve
+    ]);
 
     const totalPosts = await Post.countDocuments();
 
@@ -71,6 +71,7 @@ export const getposts = async (req, res, next) => {
     next(error);
   }
 };
+
 
 export const deletepost = async (req, res, next) => {
   if (!req.user.isAdmin || req.user.id !== req.params.userId) {
