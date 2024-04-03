9d0c71e190d47ddd89eb1c682d6493f816f5b48c
thebigjoe10 <jojosr001@gmail.com>
2024-04-04 00:18:51 +0100

    Update DashPost.jsx

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -15,7 +15,7 @@ export default function DashPosts() {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const res = await fetch(`/api/post/gethomepageposts?userId=${currentUser._id}`);
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
