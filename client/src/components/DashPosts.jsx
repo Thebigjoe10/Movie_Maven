60b2d2c614539521663544a43f890d477a2e9011
Josy <110489359+Thebigjoe10@users.noreply.github.com>
2024-04-06 21:43:32 +0100

    Update DashPosts.jsx

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -36,7 +36,7 @@ export default function DashPosts() {
     const startIndex = userPosts.length;
     try {
       const res = await fetch(
-        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+        `/api/post/gethomepageposts?userId=${currentUser._id}&startIndex=${startIndex}`
       );
       const data = await res.json();
       if (res.ok) {
