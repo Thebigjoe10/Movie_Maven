773fc5165a325ba52c6e44080ccb90a22249ab7a
thebigjoe10 <jojosr001@gmail.com>
2024-04-09 00:35:22 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import { HelmetProvider } from "react-helmet-async";
+import { HelmetProvider } from "react-helmet";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
