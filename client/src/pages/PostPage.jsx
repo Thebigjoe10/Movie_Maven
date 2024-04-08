0e46815266505168a205062f31ce7b33961704d9
thebigjoe10 <jojosr001@gmail.com>
2024-04-09 00:30:15 +0100

    Update Postpage.jsx

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { Helmet } from "react-helmet-async";
+import { Helmet } from "react-helmet";
 import { Button, Spinner } from "flowbite-react";
 import PostCard from "../components/PostCard";
 import CommentSection from "../components/CommentSection";
@@ -53,8 +53,12 @@ const PostPage = () => {
 
         if (res.ok) {
           const filteredRelatedPosts = data.posts
-            .filter(relatedPost => relatedPost._id !== post._id)
-            .filter(relatedPost => relatedPost.category === post.category && relatedPost.genre === post.genre)
+            .filter((relatedPost) => relatedPost._id !== post._id)
+            .filter(
+              (relatedPost) =>
+                relatedPost.category === post.category &&
+                relatedPost.genre === post.genre
+            )
             .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
 
           setRelatedPosts(filteredRelatedPosts);
@@ -74,7 +78,9 @@ const PostPage = () => {
           return;
         }
 
-        const res = await fetch(`/api/post/getposts?category=${post.category}&limit=5`);
+        const res = await fetch(
+          `/api/post/getposts?category=${post.category}&limit=5`
+        );
         const data = await res.json();
 
         if (res.ok) {
@@ -91,7 +97,12 @@ const PostPage = () => {
   }, [post]);
 
   if (loading) {
-    return <Spinner size="xl" className="flex justify-center items-center min-h-screen" />;
+    return (
+      <Spinner
+        size="xl"
+        className="flex justify-center items-center min-h-screen"
+      />
+    );
   }
 
   if (error) {
@@ -109,7 +120,9 @@ const PostPage = () => {
           <>
             <meta charSet="utf-8" />
             <title>{post.title}</title>
-              <meta name="keywords" content="movies, series, anime, kdrama, reviews, moviemaven movies, entertainment, nkiri, moviemaven xyz, moviemaven,nkiri.com, movie box pro download, netnaija movie download, waploaded, Movies,
+            <meta
+              name="keywords"
+              content="movies, series, anime, kdrama, reviews, moviemaven movies, entertainment, nkiri, moviemaven xyz, moviemaven,nkiri.com, movie box pro download, netnaija movie download, waploaded, Movies,
 tamil movies,
 bollywood movies,
 iron man,
@@ -129,16 +142,23 @@ moviemaven - ultimate source,
 deadpool,
 spider man movies,
 telugu movies on line,
-full movie on line" />
+full movie on line"
+            />
             <meta name="author" content="MovieMaven" />
             <meta name="description" content={post.content} />
             <meta name="robots" content="index, follow" />
-            <link rel="canonical" href={`https://www.moviemaven.xyz/post/getposts?slug=${postSlug}`} />
+            <link
+              rel="canonical"
+              href={`https://www.moviemaven.xyz/post/getposts?slug=${postSlug}`}
+            />
             <meta property="og:type" content="article" />
             <meta property="og:title" content={post.title} />
             <meta property="og:description" content={post.content} />
             <meta property="og:image" content={post.image} />
-            <meta property="og:url" content={`https://www.moviemaven.xyz/post/getposts?slug=${postSlug}`} />
+            <meta
+              property="og:url"
+              content={`https://www.moviemaven.xyz/post/getposts?slug=${postSlug}`}
+            />
             <meta property="og:site_name" content="MovieMaven" />
             <meta property="og:locale" content="en_US" />
             <meta name="twitter:card" content="summary_large_image" />
@@ -146,31 +166,50 @@ full movie on line" />
             <meta name="twitter:title" content={post.title} />
             <meta name="twitter:description" content={post.content} />
             <meta name="twitter:image" content={post.image} />
-            <meta name="twitter:url" content={`https://www.moviemaven.xyz/post/getposts?slug=${postSlug}`} />
+            <meta
+              name="twitter:url"
+              content={`https://www.moviemaven.xyz/post/getposts?slug=${postSlug}`}
+            />
           </>
         )}
       </Helmet>
 
       <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
-        <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">{post.title}</h1>
-        <Link to={`/search?category=${post.category}`} className="self-center mt-5">
-          <Button color="gray" pill size="xs">{post.category}</Button>
+        <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
+          {post.title}
+        </h1>
+        <Link
+          to={`/search?category=${post.category}`}
+          className="self-center mt-5">
+          <Button color="gray" pill size="xs">
+            {post.category}
+          </Button>
         </Link>
         {post.genre && (
           <Link to={`/search?genre=${post.genre}`} className="self-center mt-5">
-            <Button color="gray" pill size="xs">{post.genre}</Button>
+            <Button color="gray" pill size="xs">
+              {post.genre}
+            </Button>
           </Link>
         )}
-        <img src={post.image} alt={post.title} className="mt-10 p-3 h-full w-full object-cover" />
+        <img
+          src={post.image}
+          alt={post.title}
+          className="mt-10 p-3 h-full w-full object-cover"
+        />
         <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
           <span>{new Date(post.createdAt).toLocaleDateString()}</span>
-          <span className="italic">{(post.content.length / 1000).toFixed(0)} mins read</span>
+          <span className="italic">
+            {(post.content.length / 1000).toFixed(0)} mins read
+          </span>
         </div>
-        <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{ __html: post.content }}></div>
+        <div
+          className="p-3 max-w-2xl mx-auto w-full post-content"
+          dangerouslySetInnerHTML={{ __html: post.content }}></div>
         <div className="flex flex-col justify-center items-center mb-5">
           <h1 className="text-xl mt-5">Related {post.genre}</h1>
           <div className="flex flex-wrap gap-5 mt-5 justify-center">
-            {relatedPosts.map(relatedPost => (
+            {relatedPosts.map((relatedPost) => (
               <PostCard key={relatedPost._id} post={relatedPost} />
             ))}
           </div>
@@ -178,7 +217,7 @@ full movie on line" />
         <div className="flex flex-col justify-center items-center mb-5">
           <h1 className="text-xl mt-5">YOU MIGHT ALSO LIKE</h1>
           <div className="flex flex-wrap gap-5 mt-5 justify-center">
-            {recommendedPosts.map(recommendedPost => (
+            {recommendedPosts.map((recommendedPost) => (
               <PostCard key={recommendedPost._id} post={recommendedPost} />
             ))}
           </div>
