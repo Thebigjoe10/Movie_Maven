87db4f3acbc1445407e77d90ea6e3419ae796ae2
thebigjoe10 <jojosr001@gmail.com>
2024-04-13 12:00:55 +0100

    Update

--- api/index.js
+++ api/index.js
@@ -19,12 +19,25 @@ mongoose
     console.log(err);
   });
   
-  const __dirname = path.resolve();
-  
-  const app = express();
-  
-  app.use(express.json());
-  app.use(cookieParser());
+const __dirname = path.resolve();
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+// Define middleware function to set Cache-Control headers
+const setCacheControl = (req, res, next) => {
+  // Check if the request URL is the root or other HTML pages except /dmca-validation.html
+  if (req.originalUrl === '/' || req.originalUrl.endsWith('.html')) {
+    res.setHeader('Cache-Control', 'public, max-age=3600'); // Cache HTML for 1 hour
+  } else if (req.originalUrl !== '/dmca-validation.html') {
+    res.setHeader('Cache-Control', 'public, max-age=604800'); // Cache other assets for 1 week
+  }
+  next();
+};
+
+// Apply middleware to set Cache-Control headers for relevant routes
+app.use(setCacheControl);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
@@ -35,7 +48,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-
+// Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('/dmca-validation.html', (req, res) => {
@@ -45,6 +58,7 @@ app.get('/dmca-validation.html', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
