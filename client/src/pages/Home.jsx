7808485ce1507662d5c9a1e56e58bdd39b2acbd2
Josy <110489359+Thebigjoe10@users.noreply.github.com>
2024-04-19 13:44:38 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -73,7 +73,7 @@ export default function Home() {
       </div>
 
       <h2 className="text-2xl font-semibold text-center py-4">What To Watch?</h2> 
-      <Slider autoplay={true} autoplaySpeed={2000} arrows={true}>
+      <Slider autoplay={true} autoplaySpeed={2000} arrows={true} style={{ width: "90%", margin: "0 auto" }}>
         {reviews && reviews.length > 0 && reviews.map((post) => (
           <div className="flex flex-col gap-6" key={post._id}>
             <SwiperCard post={post} />
