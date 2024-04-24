e13f6af5b8a7275e1d8bdcb1c9c7c2ec806f7e2c
Josy <110489359+Thebigjoe10@users.noreply.github.com>
2024-04-25 00:35:40 +0100

    Update DashPosts.jsx

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -101,27 +101,38 @@ export default function DashPosts() {
       {currentUser.isAdmin && userPosts.length > 0 ? (
         <>
           <Table hoverable className='shadow-md'>
-            {/* Table Head */}
             <Table.Head>
               <Table.HeadCell>Date updated</Table.HeadCell>
               <Table.HeadCell>Post image</Table.HeadCell>
               <Table.HeadCell>Post title</Table.HeadCell>
               <Table.HeadCell>Category</Table.HeadCell>
               <Table.HeadCell>Delete</Table.HeadCell>
-              <Table.HeadCell>Edit</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
             </Table.Head>
-            {/* Table Body */}
             {userPosts.map((post) => (
-              <Table.Body key={post._id}>
-                <Table.Row>
-                  <Table.Cell>{new Date(post.updatedAt).toLocaleDateString()}</Table.Cell>
+              <Table.Body className='divide-y' key={post._id}>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
                   <Table.Cell>
                     <Link to={`/post/${post.slug}`}>
-                      <img src={post.image} alt={post.title} className='w-20 h-10 object-cover bg-gray-500' />
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className='w-20 h-10 object-cover bg-gray-500'
+                      />
                     </Link>
                   </Table.Cell>
                   <Table.Cell>
-                    <Link className='font-medium text-gray-900' to={`/post/${post.slug}`}>{post.title}</Link>
+                    <Link
+                      className='font-medium text-gray-900 dark:text-white'
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
                   </Table.Cell>
                   <Table.Cell>{post.category}</Table.Cell>
                   <Table.Cell>
@@ -136,8 +147,11 @@ export default function DashPosts() {
                     </span>
                   </Table.Cell>
                   <Table.Cell>
-                    <Link className='text-teal-500 hover:underline' to={`/update-post/${post._id}`}>
-                      Edit
+                    <Link
+                      className='text-teal-500 hover:underline'
+                      to={`/update-post/${post._id}`}
+                    >
+                      <span>Edit</span>
                     </Link>
                   </Table.Cell>
                 </Table.Row>
@@ -145,13 +159,17 @@ export default function DashPosts() {
             ))}
           </Table>
           {showMore && (
-            <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>Show more</button>
+            <button
+              onClick={handleShowMore}
+              className='w-full text-teal-500 self-center text-sm py-7'
+            >
+              Show more
+            </button>
           )}
         </>
       ) : (
         <p>You have no posts yet!</p>
       )}
-      {/* Modal */}
       <Modal
         show={showModal}
         onClose={() => setShowModal(false)}
@@ -161,17 +179,21 @@ export default function DashPosts() {
         <Modal.Header />
         <Modal.Body>
           <div className='text-center'>
-            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 mb-4 mx-auto' />
-            <h3 className='mb-5 text-lg text-gray-500'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
               Are you sure you want to delete this post?
             </h3>
             <div className='flex justify-center gap-4'>
-              <Button color='failure' onClick={handleDeletePost}>Yes, I'm sure</Button>
-              <Button color='gray' onClick={() => setShowModal(false)}>No, cancel</Button>
+              <Button color='failure' onClick={handleDeletePost}>
+                Yes, I'm sure
+              </Button>
+              <Button color='gray' onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
             </div>
           </div>
         </Modal.Body>
       </Modal>
     </div>
-  );
-}
+  )
+};
