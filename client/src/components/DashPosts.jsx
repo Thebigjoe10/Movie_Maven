4707105b6c8e97395448d26b373b325cec3b7440
Josy <110489359+Thebigjoe10@users.noreply.github.com>
2024-04-24 23:50:48 +0100

    Update DashPosts.jsx

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -99,58 +99,78 @@ export default function DashPosts() {
         />
         <Button onClick={handleSearch} gradientDuoTone="purpleToBlue">Search</Button>
       </div>
-      {currentUser.isAdmin && (
-        <Table hoverable className='shadow-md'>
-          {/* Table Head */}
-          <Table.Head>
-            <Table.HeadCell>Date updated</Table.HeadCell>
-            <Table.HeadCell>Post image</Table.HeadCell>
-            <Table.HeadCell>Post title</Table.HeadCell>
-            <Table.HeadCell>Category</Table.HeadCell>
-            <Table.HeadCell>Delete</Table.HeadCell>
-            <Table.HeadCell>Edit</Table.HeadCell>
-          </Table.Head>
-          {/* Table Body */}
-          {userPosts.map((post) => (
-            <Table.Body key={post._id}>
-              <Table.Row>
-                <Table.Cell>{new Date(post.updatedAt).toLocaleDateString()}</Table.Cell>
-                <Table.Cell>
-                  <Link to={`/post/${post.slug}`}>
-                    <img src={post.image} alt={post.title} className='w-20 h-10 object-cover bg-gray-500' />
-                  </Link>
-                </Table.Cell>
-                <Table.Cell>
-                  <Link className='font-medium text-gray-900' to={`/post/${post.slug}`}>{post.title}</Link>
-                </Table.Cell>
-                <Table.Cell>{post.category}</Table.Cell>
-                <Table.Cell>
-                  <span
-                    onClick={() => {
-                      setShowModal(true);
-                      setPostIdToDelete(post._id);
-                    }}
-                    className='font-medium text-red-500 hover:underline cursor-pointer'
-                  >
-                    Delete
-                  </span>
-                </Table.Cell>
-                <Table.Cell>
-                  <Link className='text-teal-500 hover:underline' to={`/update-post/${post._id}`}>
-                    Edit
-                  </Link>
-                </Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          ))}
-        </Table>
+            {currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table hoverable className='shadow-md'>
+            <Table.Head>
+              <Table.HeadCell>Date updated</Table.HeadCell>
+              <Table.HeadCell>Post image</Table.HeadCell>
+              <Table.HeadCell>Post title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            {userPosts.map((post) => (
+              <Table.Body className='divide-y' key={post._id}>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className='w-20 h-10 object-cover bg-gray-500'
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className='font-medium text-gray-900 dark:text-white'
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>{post.category}</Table.Cell>
+                  <Table.Cell>
+                    <span
+                      onClick={() => {
+                        setShowModal(true);
+                        setPostIdToDelete(post._id);
+                      }}
+                      className='font-medium text-red-500 hover:underline cursor-pointer'
+                    >
+                      Delete
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className='text-teal-500 hover:underline'
+                      to={`/update-post/${post._id}`}
+                    >
+                      <span>Edit</span>
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className='w-full text-teal-500 self-center text-sm py-7'
+            >
+              Show more
+            </button>
+          )}
+        </>
+      ) : (
+        <p>You have no posts yet!</p>
       )}
-      {loading && <p>Loading...</p>}
-      {!loading && userPosts.length === 0 && <p>You have no posts yet!</p>}
-      {showMore && (
-        <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>Show more</button>
-      )}
-      {/* Modal */}
       <Modal
         show={showModal}
         onClose={() => setShowModal(false)}
@@ -160,17 +180,20 @@ export default function DashPosts() {
         <Modal.Header />
         <Modal.Body>
           <div className='text-center'>
-            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 mb-4 mx-auto' />
-            <h3 className='mb-5 text-lg text-gray-500'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
               Are you sure you want to delete this post?
             </h3>
             <div className='flex justify-center gap-4'>
-              <Button color='failure' onClick={handleDeletePost}>Yes, I'm sure</Button>
-              <Button color='gray' onClick={() => setShowModal(false)}>No, cancel</Button>
+              <Button color='failure' onClick={handleDeletePost}>
+                Yes, I'm sure
+              </Button>
+              <Button color='gray' onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
             </div>
           </div>
         </Modal.Body>
       </Modal>
     </div>
   );
-}
