b130f34eb1e947d2d10c64dddd7edbbffcedd800
Josy <110489359+Thebigjoe10@users.noreply.github.com>
2024-05-22 02:31:30 +0100

    Update post.route.js

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create, deletepost, getposts, updatepost, gethomepageposts, getfeaturedposts, searchPosts } from '../controllers/post.controller.js';
+import { create, deletepost, getposts, updatepost, gethomepageposts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create);
 router.get('/getposts', getposts);
 router.get('/gethomepageposts', gethomepageposts);
-router.get('/search', searchPosts);
 router.delete('/deletepost/:postId/:userId', verifyToken, deletepost);
 router.put('/updatepost/:postId/:userId', verifyToken, updatepost);
 
